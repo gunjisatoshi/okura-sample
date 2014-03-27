3155253a79cad6d44dd5372e6d1970a15b0524ec
Satoshi GUNJI <gunjisatoshi@gmail.com>
2014-03-27 09:10:04 +0900

    sharing tagger object

--- okura_sample.rb
+++ okura_sample.rb
@@ -3,14 +3,15 @@ require "sinatra/base"
 require 'haml'
 require 'okura/serializer'
 
+TAGGER = Okura::Serializer::FormatInfo.create_tagger('lib/okura-dic')
+
 class OkuraSample < Sinatra::Base
   get "/" do
     haml :index
   end
 
   post "/" do
-    tagger = Okura::Serializer::FormatInfo.create_tagger('lib/okura-dic')
-    @nodes = tagger.parse(request[:sentence])
+    @nodes = TAGGER.parse(request[:sentence])
     haml :index_post
   end
 end
